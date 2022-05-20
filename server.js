237b60ac3ebe37c1e1c1c48e207a55fccc302f37
Jay Agrawal <jay7697agrawal@gmail.com>
2022-05-20 11:03:48 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-app.use(express.static(__dirname + "/dist/puja-pratham-frontend"));
-app.get("/",function(req,res) { res.sendFile(path.join(__dirname + "index.html")); });
+app.use(express.static('./dist/puja_pratham_frontend'));
+app.get('/',(req,res)=>{
+    res.sendFile('index.html',{root:'dist/puja_pratham_frontend'})
+});
 app.listen(process.env.PORT || 8080);
